cb99aae157a31fe044f420e3490046506b2cc2d2
Meraki_DNA <dononichols@gmail.com>
2022-07-15 17:38:10 -0400

    Added BMI calculator

--- calculator.js
+++ calculator.js
@@ -23,6 +23,21 @@ app.post("/", function(req, res){
     res.send("The results of the calculation is " + result );
 });
 
+/////
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+//// bmi action
+app.post("/bmiCalculator", function(req, res){
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height * height);
+
+    res.send("Your BMI is" + bmi);
+});
+
 
 app.listen(3000, function() {
     console.log("Server is running on port 3,000.");
