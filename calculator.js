564310fc7bc6a7b0ea986e719dbed5969c4bf861
Meraki_DNA <dononichols@gmail.com>
2022-07-15 16:56:11 -0400

    -installed body-parser w/ npm install body-parser

--- calculator.js
+++ calculator.js
@@ -8,6 +8,12 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
+
+app.post("/", function(req, res){
+    res.send("Thanks for posting that!");
+});
+
+
 app.listen(3000, function() {
     console.log("Server is running on port 3,000.");
 });
